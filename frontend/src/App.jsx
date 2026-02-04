079d09960c52f3930cc4573b63af9cb4be671ff3
Youssef Dammak <149438924+eltouj@users.noreply.github.com>
2026-02-04 09:50:26 +0100

    routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
-import Login from './apps/hr/login/Login.jsx'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import HrLogin from './apps/hr/login/Login.jsx'
+import CandidateLogin from './apps/candidat/Login/LoginPage.jsx'
 
 function App() {
   return (
     <div className="app">
-      <Login />
+      <Routes>
+        {/* Route candidat login (route principale) */}
+        <Route path="/candidat/login" element={<CandidateLogin />} />
+
+        {/* Route HR login (séparée, pas route principale) */}
+        <Route path="/hr/login" element={<HrLogin />} />
+
+        {/* Route racine -> redirection vers candidat/login */}
+        <Route path="/" element={<Navigate to="/candidat/login" replace />} />
+
+        {/* Fallback pour toutes les autres URLs */}
+        <Route path="*" element={<Navigate to="/candidat/login" replace />} />
+      </Routes>
     </div>
   )
 }
 
-export default App;
+export default App
 
